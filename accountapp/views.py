ddb9d36df0e67d5cef21dc33dc64f7ebe20c267c
Nuri Lee <leenuri67@gmail.com>
2023-12-06 18:18:42 +0900

    post로 저장한 후 get으로 redirect

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from accountapp.models import HelloWorld
 
@@ -18,7 +19,10 @@ def hello_world(request):
 
         hello_world_list = HelloWorld.objects.all()
         # return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+        # return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list}) -> 새로고침하면 계속 저장됨
+
+        # accountapp 내부에 있는 hello_wordl로 재접속하라. 새로고침해도 저장되지 않는다.
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
         hello_world_list = HelloWorld.objects.all()
         # return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
