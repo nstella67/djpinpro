ec232dea3d19ccf099d12d64ebb8c70d30354c4d
Nuri Lee <leenuri67@gmail.com>
2023-12-11 15:51:05 +0900

    decorator 이용한 Authentication

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,39 +1,44 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorators import account_ownerthip_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
+has_ownership = [login_required, account_ownerthip_required]
 
 # Create your views here.
 
+@login_required
 def hello_world(request):
     # return HttpResponse('Hello world!')
-    if request.user.is_authenticated:
-        if request.method == "POST":
+    # if request.user.is_authenticated: -> @login_required로 decorator를 이용
+    if request.method == "POST":
 
-            temp = request.POST.get('hello_world_input')
+        temp = request.POST.get('hello_world_input')
 
-            new_hello_world = HelloWorld()
-            new_hello_world.text = temp
-            new_hello_world.save()
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
 
-            # hello_world_list = HelloWorld.objects.all()
-            # return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
-            # return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list}) -> 새로고침하면 계속 저장됨
+        # hello_world_list = HelloWorld.objects.all()
+        # return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+        # return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list}) -> 새로고침하면 계속 저장됨
 
-            # accountapp 내부에 있는 hello_wordl로 재접속하라. 새로고침해도 저장되지 않는다.
-            return HttpResponseRedirect(reverse('accountapp:hello_world'))
-        else:
-            hello_world_list = HelloWorld.objects.all()
-            # return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
-            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+        # accountapp 내부에 있는 hello_wordl로 재접속하라. 새로고침해도 저장되지 않는다.
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return HttpResponseRedirect(reverse('accountapp:login'))
+        hello_world_list = HelloWorld.objects.all()
+        # return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+    # else:
+    #     return HttpResponseRedirect(reverse('accountapp:login'))
 
 class AccountCreateView(CreateView):
     model = User
@@ -46,6 +51,13 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
+# @method_decorator(login_required, 'get')
+# @method_decorator(login_required, 'post')
+# @method_decorator(account_ownerthip_required, 'get')
+# @method_decorator(account_ownerthip_required, 'post')
+# login_required, account_ownerthip_required 배열에 넣어서 아래 두줄로 만들 수 있다
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
@@ -53,32 +65,34 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
-    def get(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().pet(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
+    # def get(self, *args, **kwargs):
+    #     if self.request.user.is_authenticated and self.get_object() == self.request.user:
+    #         return super().pet(*args, **kwargs)
+    #     else:
+    #         return HttpResponseForbidden()
+    #
+    # def post(self, *args, **kwargs):
+    #     if self.request.user.is_authenticated and self.get_object() == self.request.user:
+    #         return super().post(*args, **kwargs)
+    #     else:
+    #         return HttpResponseForbidden()
 
-    def post(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().post(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/delete.html'
 
-    def get(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().pet(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
-
-    def post(self, *args, **kwargs):
-        if self.request.user.is_authenticated and self.get_object() == self.request.user:
-            return super().post(*args, **kwargs)
-        else:
-            return HttpResponseForbidden()
+    # def get(self, *args, **kwargs):
+    #     if self.request.user.is_authenticated and self.get_object() == self.request.user:
+    #         return super().pet(*args, **kwargs)
+    #     else:
+    #         return HttpResponseForbidden()
+    #
+    # def post(self, *args, **kwargs):
+    #     if self.request.user.is_authenticated and self.get_object() == self.request.user:
+    #         return super().post(*args, **kwargs)
+    #     else:
+    #         return HttpResponseForbidden()
