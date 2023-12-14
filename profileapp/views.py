c0a97a898731e5cded82592257752ec34b9773e4
Nuri Lee <leenuri67@gmail.com>
2023-12-14 10:58:20 +0900

    프로필 생성/수정 후 success url

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
@@ -15,7 +15,8 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    # success_url = reverse_lazy('accountapp:hello_world')
+    # 성공 후 hello_world가 아니라 detail로 넘겨주고 싶은데, pk 넘길 수 없음. get_success_url 메서드 추가해준다
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form):
@@ -25,11 +26,17 @@ class ProfileCreateView(CreateView):
 
         return super().form_valid(form)
 
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
+
 @method_decorator(profile_ownerthip_required, 'get')
 @method_decorator(profile_ownerthip_required, 'post')
 class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    # success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
