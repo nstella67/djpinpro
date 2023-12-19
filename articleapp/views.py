c967b4268cd0ea78bc5418fc203ab1d00ab07a93
Nuri Lee <leenuri67@gmail.com>
2023-12-19 10:33:05 +0900

    article list 개수

--- articleapp/views.py
+++ articleapp/views.py
@@ -64,4 +64,4 @@ class ArticleListView(ListView):
     model = Article
     context_object_name = 'article_list'
     template_name = 'articleapp/list.html'
-    paginate_by = 7
+    paginate_by = 5
